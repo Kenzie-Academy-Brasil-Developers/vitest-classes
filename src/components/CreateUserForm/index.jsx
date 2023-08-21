f855c56487ee40b2473392352716b51f939d596a
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-08-21 11:42:31 -0300

    removing useContext (should not be in the next class)

--- src/components/CreateUserForm/index.jsx
+++ src/components/CreateUserForm/index.jsx
@@ -1,16 +1,6 @@
-import { useContext } from "react";
-import { UsersContext } from "../../providers/UsersContext";
-
 export const CreateUserForm = () => {
-   const { createUser } = useContext(UsersContext);
-
-   const submit = (event) => {
-      event.preventDefault();
-      createUser();
-   };
-
    return (
-      <form onSubmit={submit} role="form">
+      <form role="form">
          <input type="text" placeholder="Nome" />
          <input type="text" placeholder="E-mail" />
          <button>Cadastrar usuário</button>
