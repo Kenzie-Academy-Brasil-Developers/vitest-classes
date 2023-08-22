a973068ea6525599694310b9dc40c54e1d9cec04
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-08-22 11:56:49 -0300

    testing input and submit behavior in form

--- src/components/CreateUserForm/CreateUserForm.test.jsx
+++ src/components/CreateUserForm/CreateUserForm.test.jsx
@@ -1,18 +1,20 @@
 import { fireEvent, render, screen } from "@testing-library/react";
-import { describe, expect, test, vi } from "vitest";
+import { beforeEach, describe, expect, test, vi } from "vitest";
 import { UsersContext } from "../../providers/UsersContext";
 import { CreateUserForm } from ".";
 
 describe("CreateUserForm", () => {
    const createUserMock = vi.fn();
 
-   test("should render correctly", () => {
+   beforeEach(() => {
       render(
          <UsersContext.Provider value={{ createUser: createUserMock }}>
             <CreateUserForm />
          </UsersContext.Provider>
       );
+   });
 
+   test("should render correctly", () => {
       const form = screen.getByRole("form");
       expect(form).toBeInTheDocument();
 
@@ -24,5 +26,28 @@ describe("CreateUserForm", () => {
 
       const submitButton = screen.getByText("Cadastrar usuário");
       expect(submitButton).toBeInTheDocument();
+   });
+
+   test("inputs are working correctly", () => {
+      const userMock = {
+         name: "John doe",
+         email: "johndoe@email.com",
+      };
+
+      const name = screen.getByPlaceholderText("Nome");
+      const email = screen.getByPlaceholderText("E-mail");
+
+      fireEvent.change(name, { target: { value: userMock.name } });
+      fireEvent.change(email, { target: { value: userMock.email } });
+
+      expect(name).toHaveValue(userMock.name);
+      expect(email).toHaveValue(userMock.email);
+   });
+
+   test("submit function should fire correctly", () => {
+    const submitButton = screen.getByText("Cadastrar usuário");
+    fireEvent.click(submitButton);
+
+    expect(createUserMock).toBeCalledTimes(1);
    });
 });
--- src/components/CreateUserForm/index.jsx
+++ src/components/CreateUserForm/index.jsx
@@ -1,6 +1,16 @@
+import { useContext } from "react";
+import { UsersContext } from "../../providers/UsersContext";
+
 export const CreateUserForm = () => {
+   const { createUser } = useContext(UsersContext);
+   
+   const submit = (event) => {
+    event.preventDefault();
+    createUser();
+   }
+
    return (
-      <form role="form">
+      <form onSubmit={submit} role="form">
          <input type="text" placeholder="Nome" />
          <input type="text" placeholder="E-mail" />
          <button>Cadastrar usuário</button>
